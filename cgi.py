83da7d788727d707f6b3e538be1919d8768abfe0
yonatan beyn <Natan09@yahoo.com>
2021-01-06 22:14:53 -0600

    Update cgi.py
    i will update the program. still in development

--- cgi.py
+++ cgi.py
@@ -1,45 +1,22 @@
-import cgi
+import cgi# for processing user input specifically forms
 import socket
 HOST = 'localhost'
 PORT = 12345
 BUFSIZ = 1024
-ADDR = (HOST, PORT)
-if __name__ == '__main__':
-    server_socket = socket.socket(socket.AF_INET,
-                                  socket.SOCK_STREAM)
-    server_socket.bind(ADDR)
-    server_socket.listen(5)
-    server_socket.setsockopt( socket.SOL_SOCKET,
-    socket.SO_REUSEADDR, 1 )
-    while True:
-        print('Server waiting for connection...')
-        client_sock, addr = server_socket.accept()
-        print('Client connected from: ', addr)
-        while True:
-            data = client_sock.recv(BUFSIZ)
-            #print the received data to check the request type and it's header file
-            print("Received from client: %s" % data.decode('utf-8'))
-            #print(data)
-           # if not data or data.decode('utf-8') == 'END':
-               # break
-       
-            #data to be sended
-            data1="""Content-type:text/html\r\n\r\n"""
-            data2="""<html>"""
-            data3="""<body>"""
-        
-            data4="""<p> it works</p>"""
-            data5="""</body>"""
-            data6="""</html>"""
-            try:
-                client_sock.send(bytes(data1, 'utf-8'))
-                client_sock.send(bytes(data2, 'utf-8'))
-                client_sock.send(bytes(data3, 'utf-8'))
-                client_sock.send(bytes(data4, 'utf-8'))
-                client_sock.send(bytes(data5, 'utf-8'))
-                client_sock.send(bytes(data6, 'utf-8')) 
-            
-            except KeyboardInterrupt:
-                print("program exiting")
-        client_sock.close()
-    server_socket.close()
+class WebServer():
+    """web server based on a socket. http capsulation """
+    def __init__(self,port=8080):#default port for web server
+        self.port=port
+        self.host=HOST
+    def sock_start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            print("Starting server on {host}:{port}".format(host=self.host, port=self.port))#log to the console
+            self.socket.bind((self.host, self.port))
+            print("Server started on port {port}.".format(port=self.port))
+
+        except Exception as e:
+            print("Error: Could not bind to port {port}".format(port=self.port))
+            self.shutdown()
+            sys.exit(1)
+    
